a8f9e73b72820aae451494c8a4d8c7c1e82608e9
Administrator <Administrator@172.31.3.64>
2024-03-08 07:31:27 +0000

    Adding all files of my selenium project.

---
+++ Sel_0308/src/main/java/Selenium_0308/Locators.java
@@ -0,0 +1,29 @@
+package Selenium_0308;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+public class Locators {
+
+	static WebDriver driver = new ChromeDriver();
+
+	public static void InvokeBrowser() {
+		driver.get("C:\\Users\\Administrator\\eclipse-workspace\\Sel_0308\\htmlfile.html");
+		driver.manage().window().maximize();
+		By maleradiobutton = RelativeLocator.with(By.tagName("input")).toLeftOf(By.cssSelector("input[value='f']"));
+		driver.findElement(maleradiobutton).click();
+	}
+
+	/*
+	 * public static void Locators() { By maleradiobutton =
+	 * RelativeLocator.with(By.tagName("input")).toLeftOf(By.xpath(
+	 * "//input[@value='f']")); driver.findElement(maleradiobutton).click(); }
+	 */
+	public static void main(String[] args) {
+
+		InvokeBrowser();
+	}
+
+}
