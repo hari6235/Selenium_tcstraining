47c72a7af5f29c4cd46dc99319923a24d73d8b2e
Administrator <Administrator@172.31.13.37>
2024-03-11 06:09:47 +0000

    New code merge20311

---
+++ Sel_0308/src/main/java/differentElements/ActiveElements.java
@@ -0,0 +1,23 @@
+package differentElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ActiveElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+	      try {
+	        driver.get("http://www.google.com");
+	        driver.findElement(By.cssSelector("[name='q']")).sendKeys("webElement");
+
+	        // Get attribute of current active element
+	        String attr = driver.switchTo().activeElement().getAttribute("title");
+	        System.out.println(attr);
+	      } finally {
+	          driver.quit();
+	}
+	}
+}
