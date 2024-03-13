fc10d3fde47d636371964a81ff8ec0900ec1444a
Administrator <Administrator@172.31.2.51>
2024-03-13 05:00:19 +0000

    class name change

--- Sel_0308/src/main/java/differentElements/Radio_buttons.java
+++ Sel_0308/src/main/java/differentElements/Radio_buttons.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Radion_buttons {
+public class Radio_buttons {
 
 	static WebDriver driver = new ChromeDriver();
 	
