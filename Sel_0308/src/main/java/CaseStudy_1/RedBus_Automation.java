63264a787ce44fbb89c9e61a63d0aff75886e377
Administrator <Administrator@172.31.9.242>
2024-03-16 19:04:05 +0000

    code commit

--- Sel_0308/src/main/java/CaseStudy_1/RedBus_Automation.java
+++ Sel_0308/src/main/java/CaseStudy_1/RedBus_Automation.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import TestNGpack.RunAll;
@@ -49,36 +50,44 @@ public class RedBus_Automation {
 	}
 	@Test(priority = 2)
 	public void Testcase_2() throws InterruptedException {
-		
+
 		/*---Testcase2::Sending the inputs---*/
-		
+
 		//Source Input
 		driver.findElement(By.xpath("//input[@id='src']")).click();
 		driver.findElement(By.xpath("//input[@id='src']")).sendKeys("Chennai");
 		Thread.sleep(5000);
-		
-		//Destination Input
+
+		//---Destination Input
 		driver.findElement(By.xpath("//input[@id='dest']")).click();
-		driver.findElement(By.xpath("//input[@id='dest']")).sendKeys("Hyderabad");
+		driver.findElement(By.xpath("//input[@id='dest']")).sendKeys("Banglore");
 		Thread.sleep(5000);
-		
-		//Date Picker
-		
+
+		//---Date Picker
+
 		WebElement dateInputField = driver.findElement(By.id("onwardCal"));
 		dateInputField.click();
-		
-		//Passing current date to Date Input Field
-		
+
+		//---Passing current date to Date Input Field
+
 		LocalDate currentDate = LocalDate.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
 		String formattedDate = currentDate.format(formatter);
 
 		WebElement dayElement = driver.findElement(By.xpath("//span[contains(@class,'DayTiles__CalendarDaysSpan-sc-1xum02u-1 fgdqFw') and text()='"+ formattedDate +"']"));
 		dayElement.click();
-		
-		//Search for available buses
-		
+
+		//---Search for available buses
+
 		driver.findElement(By.id("search_button")).click();
-		
-	}	
+
+	}
+
+	@AfterClass
+
+	public void teardown(){
+
+		driver.quit();
+	}
+
 }
